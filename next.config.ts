b2c16217beebf6a87bdd4349fb25bca174fb7f66
Noey Ignacio <francis.ignacio@aelluminate.com>
2024-10-29 06:59:17 +0800

    refactor: update `basePath` and `assetPrefix`

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/nextjs-github-pages",
+  basePath: process.env.GITHUB_PAGES ? "/aelluminate" : "",
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.GITHUB_PAGES ? "/aelluminate/" : "",
 }
 
 export default nextConfig
