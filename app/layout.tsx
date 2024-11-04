f3fe8de488656327e484e53a287ffaca28bdab70
Noey Ignacio <francis.ignacio@aelluminate.com>
2024-11-04 14:29:46 +0800

    refactor: move layout and page files, update styles and content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,23 @@
-import React from "react"
-import { Metadata } from "next"
-
-// Global styles & fonts
-import "../assets/globals.css"
+import "@/assets/core.css";
+import type { Metadata } from "next";
+import { metadata as SiteMetadata } from "@/config/meta/metadata"
 import { lora, lexendDeca } from "@/utils/fonts"
+import { Footer } from "@/components/";
 
-// Metadata for the global layout
-import { metadata as SiteMetadata } from "@/config/metadata"
 export const metadata: Metadata = SiteMetadata
 
-// Components
-import { Footer } from "@/components/footer/_index"
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={[lora.variable, lexendDeca.variable].join(" ")}>
       <body className={`bg-neutral-900 font-lexend-deca font-light text-slate-50 antialiased`}>
-        {children}
+          {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center overflow-hidden px-8 py-32">
+    <main className="flex h-full w-full flex-col items-center overflow-hidden px-8 py-32">
       <div className="flex flex-col items-center">
-        <span className="font-inter text-xs font-medium uppercase text-slate-400">Coming soon</span>
-        <h1 className="pt-4 font-lora text-4xl font-semibold tracking-tight">Aelluminate.</h1>
+        <span className="font-inter text-xs font-medium uppercase text-slate-400">
+          Will be available soon!
+        </span>
+        <h1 className="pt-4 font-lora text-4xl font-semibold tracking-tight">Aelluminate Labs.</h1>
         <div className="w-full max-w-xl pt-6 text-center font-lexend-deca text-sm font-light md:text-base lg:text-base">
           A forward-thinking organization that loves to empowers businesses to soar to new heights
           by providing the support and resources they need, we foster a culture of innovation and
