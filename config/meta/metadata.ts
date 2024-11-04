cfa9ad698e1c99cffa3ff44fcce93b6f25a53ac9
Noey Ignacio <francis.ignacio@aelluminate.com>
2024-11-04 14:30:35 +0800

    feat: add metadata and keywords configuration for SEO optimization

---
+++ config/meta/metadata.ts
@@ -0,0 +1,84 @@
+import { Metadata } from "next"
+import { keywords } from "./keywords"
+
+export const metadata: Metadata = {
+  title: {
+    default: "Aelluminate Labs",
+    template: "%s | Aelluminate Labs",
+  },
+  keywords: keywords,
+  manifest: "https://aelluminate.com/manifest.json",
+  generator: "Aelluminate Labs",
+  applicationName: "Aelluminate Labs",
+  description:
+    "We believe in the power of vision and dedication. Our mission is to help every business we work with to soar and turn possibilities into accomplishments.",
+  openGraph: {
+    title: "Aelluminate Labs • Loves to empowers businesses",
+    description:
+      "We believe in the power of vision and dedication. Our mission is to help every business we work with to soar and turn possibilities into accomplishments.",
+    url: "https://aelluminate.com",
+    siteName: "Aelluminate Labs",
+    images: [
+      {
+        url: "https://aelluminate.com/og.png",
+        width: 1920,
+        height: 1080,
+        alt: "Aelluminate Labs Open Graph Image",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Aelluminate",
+    card: "summary_large_image",
+    site: "@aelluminate",
+    creator: "@aelluminate",
+    creatorId: "1658936245899370503",
+    description:
+      "We believe in the power of vision and dedication. Our mission is to help every business we work with to soar and turn possibilities into accomplishments.",
+    images: [
+      {
+        url: "https://aelluminate.com/og.png",
+        width: 1920,
+        height: 1080,
+        alt: "Aelluminate Open Graph Image",
+      },
+    ],
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", sizes: "16x16" },
+    ],
+  },
+  alternates: {
+    canonical: "https://aelluminate.com",
+    languages: {},
+  },
+  verification: {
+    other: {
+      me: ["labs@aelluminate.com"],
+    },
+  },
+  appLinks: {
+    web: {
+      url: "https://aelluminate.com",
+      should_fallback: true,
+    },
+  },
+  category: "business",
+}
